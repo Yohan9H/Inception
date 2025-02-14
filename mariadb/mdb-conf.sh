c7a5d8fcb9e3dc77a3f14a98d3b9341f0808bd97
Yohan9H <yhurtebize58@gmail.com>
2025-02-14 09:33:07 +0100

    modify mdb-conf

--- mariadb/mdb-conf.sh
+++ mariadb/mdb-conf.sh
@@ -1,9 +1,11 @@
-service mariadb start
+mysqld_safe --port=3306 --bind-address=0.0.0.0 --datadir='/var/lib/mysql'
 
 sleep 5
 
-mariadb -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};"
+mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DB};"
 
-mariadb -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "CREATE USER IF NOT EXISTS \`${MYSQL_USER}\`@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
 
-mariadb -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}. * TO \`${MYSQL_USER}\`@'%';"
+mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "GRANT ALL PRIVILEGES ON ${MYSQL_DB}. * TO \`${MYSQL_USER}\`@'%';"
+
+mariadb -u root -p$MYSQL_ROOT_PASSWORD -e "FLUSH PRIVILEGES;"
